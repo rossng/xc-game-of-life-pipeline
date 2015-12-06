5d557294d3b7bac6b7e8eac84059c826f6ae9b2a
John <john.griffith@live.co.uk>
2015-12-06 21:59:08 +0000

    Remove old code

--- src/write_buffer.h
+++
@@ -1,7 +0,0 @@
-#ifndef WRITE_BUFFER_H_
-#define WRITE_BUFFER_H_
-
-void write_buffer(chanend workers[num_workers], unsigned num_workers, client interface bufswap_if i_bufswap,
-                  chanend c_rb);
-
-#endif /* WRITE_BUFFER_H_ */
